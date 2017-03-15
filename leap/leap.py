a15812b37ba68186fe72d2eb134fd345c327bea8
Kent <ktlustos@arrayschool.com>
2017-03-15 13:36:59 -0600

    Complete and submit leap exercise

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,10 @@
-def is_leap_year():
-    pass
+def is_leap_year(year):
+    leap = False
+    if year % 4 == 0:
+        if year % 100 == 0:
+            leap = False
+            if year % 400 == 0:
+                leap = True
+        else:
+            leap = True
+    return leap
